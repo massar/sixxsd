92a9b92567e7cd141621f4084c09c32eee1ee252
Jeroen Massar <jeroen@massar.ch>
2005-01-31 17:05:10 +0000

    Passwords can be upto 50 long

--- src/sixxsd.h
+++ src/sixxsd.h
@@ -3,8 +3,8 @@
  by Jeroen Massar <jeroen@sixxs.net>
 ***************************************
  $Author: jeroen $
- $Id: sixxsd.h,v 1.2 2005-01-29 14:51:35 jeroen Exp $
- $Date: 2005-01-29 14:51:35 $
+ $Id: sixxsd.h,v 1.3 2005-01-31 17:05:10 jeroen Exp $
+ $Date: 2005-01-31 17:05:10 $
 **************************************/
 
 #ifndef SIXXSD_H
@@ -134,7 +134,7 @@ struct sixxs_interface
 	// Heartbeat specific
 	// (IFACE_PROTO41_HB + IFACE_AYIYA)
 	time_t			hb_lastbeat;			// Last heartbeat we got
-	char			hb_password[50];		// Heartbeat password
+	char			hb_password[52];		// Heartbeat password
 
 	// AYIYA specific
 	// (IFACE_AYIYA)
